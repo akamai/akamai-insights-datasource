2772174d7a3cf7501896f5d7b8b3f6baf7fd5bee
mzuber <mzuber@akamai.com>
2026-05-08 09:52:17 +0200

    DPS-42725 fix vulns

---
+++ webpack.config.ts
@@ -0,0 +1,4 @@
+// This file extends the default Grafana plugin webpack configuration.
+// See: grafana.com/developers/plugin-tools/how-to-guides/extend-configurations#extend-the-webpack-config
+
+export { default } from './.config/webpack/webpack.config';
